5283fcd33c0483fc4b9660d1d7bca8ce0d6ca1a1
ZWMDR <953799126lxc@gmail.com>
2019-06-30 19:30:31 +0800

    添加math函数

--- sp_math/sp_math.c
+++ sp_math/sp_math.c
@@ -1,6 +1,11 @@
 #include "sp_math.h"
 
-u16 get_vcc(u16 *arr,u16 len,u16 start,u16 gap,float scale_rate)
+float abs_f(float num)
+{
+	return num>0?num:(-num);
+}
+
+u16 get_vcc(const u16 *arr,u16 len,u16 start,u16 gap,float scale_rate)
 {
 	u16 max=0;
 	u16 min=0xFFFF;
@@ -16,7 +21,7 @@ u16 get_vcc(u16 *arr,u16 len,u16 start,u16 gap,float scale_rate)
 	return (max-min)*scale_rate;
 }
 
-void get_vcc_duplex(u16 *arr,u16 len,u16 *VCC1,u16* VCC2)
+void get_vcc_duplex(const u16 *arr,u16 len,u16 *VCC1,u16* VCC2)
 {
 	u16 max1=0x0000;
 	u16 min1=0xFFFF;
@@ -37,4 +42,52 @@ void get_vcc_duplex(u16 *arr,u16 len,u16 *VCC1,u16* VCC2)
 	}
 	*VCC1=max1-min1;
 	*VCC2=max2-min2;
+}
+
+float find_zero_cur(const u16 *arr,u16 average,u16 len)
+{
+	u16 i;
+	float delta;
+	for(i=1;i<len;i++)
+	{
+		if(arr[i]>=average && arr[i-1]<average)
+		{
+			delta=1/(abs_f((arr[i]-average)/(average-arr[i-1]))+1);
+			return delta+i-1;
+		}
+	}
+	return 0;
+}
+
+double active_power(const u16 *voltage_arr,const u16 current_arr,u16 len,u16 voltage_ave,u16 current_ave,float voltage_rate,float current_rate)
+{
+	u16 i;
+	double sum=0;
+	double voltage,current;
+	
+	for(i=0;i<len;i++)
+	{
+		voltage=(double)(voltage_arr[i]-voltage_ave)*voltage_rate;
+		current=(double)(current_arr[i]-current_ave)*current_rate;
+		sum+=voltage*current;
+	}
+	return sum;
+}
+
+double RMS_sinusoid(const u16 *arr,u16 len,u16 average,float rate)
+{
+	double sum;
+	u16 i;
+	
+	for(i=0;i<len;i++)
+	{
+		sum+=pow((arr[i]-average)*rate,2);
+	}
+	sum/=len;
+	return sum;
+}
+
+double apparent_power(const u16 *voltage_arr,const u16 *current_arr,u16 len,u16 voltage_ave,u16 current_ave,float voltage_rate,float current_rate)
+{
+	return RMS_sinusoid(voltage_arr,len,voltage_ave,voltage_rate)*RMS_sinusoid(voltage_arr,len,voltage_ave,voltage_rate);
 }
--- sp_math/sp_math.h
+++ sp_math/sp_math.h
@@ -3,8 +3,16 @@
 #include "math.h"
 #include "sys.h"
 
-u16 get_vcc(u16 *arr,u16 len,u16 start,u16 gap,float scale_rate);
-void get_vcc_duplex(u16 *arr,u16 len,u16 *VCC1,u16* VCC2);
+float abs_f(float num);//取绝对值
+u16 get_vcc(const u16 *arr,u16 len,u16 start,u16 gap,float scale_rate);
+//获取一组数据中的峰峰值
+void get_vcc_duplex(const u16 *arr,u16 len,u16 *VCC1,u16* VCC2);//获取两路峰峰值
+float find_zero_cur(const u16 *arr,u16 average,u16 len);//找到正弦波零点(斜率大于0)，手动输入均值
 
+double RMS_sinusoid(const u16 *arr,u16 len,u16 average,float rate);
+double active_power(const u16 *voltage_arr,const u16 *current_arr,u16 len,u16 voltage_ave,
+u16 current_ave,float voltage_rate,float current_rate);//有功功率计算
+double apparent_power(const u16 *voltage_arr,const u16 *current_arr,u16 len,u16 voltage_ave,
+u16 current_ave,float voltage_rate,float current_rate);//计算视在功率
 
 #endif
